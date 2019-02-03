2d6c3be041bbade67b52b2c09adf95d6859d6901
pioka <p10mirror2@gmail.com>
2019-02-03 17:24:24 +0900

    :bug: Fix behavior at backup

--- script/backup.sh
+++ script/backup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 GAME_DIR=`dirname $0`/../game
-BACKUP_DIR=$GAME_DIR/backup
+BACKUP_DIRNAME=backups
 
+mkdir -p $GAME_DIR/$BACKUP_DIRNAME
+pushd $GAME_DIR > /dev/null
 
 # Is server running
 if screen -list | grep -q 'mcs'; then
     screen -r mcs -X stuff "save-off\rsave-all\r"
     sleep 1
-    tar zcf $BACKUP_DIR/world-`date '+%Y-%m%d-%H%M'`.tar.gz $GAME_DIR/world
+    tar -zcf $BACKUP_DIRNAME/bu-`date '+%Y-%m%d-%H%M'`.tar.gz world 
     screen -r mcs -X stuff "save-on\r"
 else
-    tar zcf $BACKUP_DIR/world-`date '+%Y-%m%d-%H%M'`.tar.gz $GAME_DIR/world
+    tar -zcf $BACKUP_DIRNAME/bu-`date '+%Y-%m%d-%H%M'`.tar.gz world
 fi
+
+popd > /dev/null
